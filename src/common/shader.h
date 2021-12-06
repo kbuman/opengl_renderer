30edd9ca1362defefc5d92540bf578298c55afaa
Kevin Buman <kevin.buman@students.fhnw.ch>
2021-12-06 14:34:41 +0100

    starter code translated to cpp

---
+++ src/common/shader.h
@@ -0,0 +1,10 @@
+//
+// Created by kevin on 06/12/2021.
+//
+
+#ifndef OPENGL_CPP_COMMON_SHADER_H_
+#define OPENGL_CPP_COMMON_SHADER_H_
+
+GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path);
+
+#endif //OPENGL_CPP_COMMON_SHADER_H_
